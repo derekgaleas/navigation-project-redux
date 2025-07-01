cd5bd310bd67cca531de3296d458195255d45311
derekgaleas <derek.galeas@sanservices.hn>
2025-07-01 11:20:24 -0600

    reducer and favoriteslice

--- store/redux/store.js
+++ store/redux/store.js
@@ -1,6 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit'
 
-export const store = configureStore({
-  reducer: {}
-});
+import favoritesReducer from './favorite';
 
+export const store = configureStore({
+  reducer: {
+    favoriteMeals: favoritesReducer
+  }
+});
